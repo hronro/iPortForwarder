95c203fe5eb412b9d774021b8914836dbba93d72
Nano Miratus <nanomiratus@gmail.com>
2025-03-21 07:22:41 +0100

    :bug: fix config file read on startup

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+- Fix loading configuration file on startup.
+
 ## [1.0.0]
 
 ### Added
--- ui/iPortForwarder/iPortForwarderApp.swift
+++ ui/iPortForwarder/iPortForwarderApp.swift
@@ -5,25 +5,48 @@ import Libipf
 @main
 struct iPortForwarderApp: App {
     @AppStorage("loadConfigurationsOnStartup") private var loadConfigurationsOnStartup = false
-    @AppStorage("configurationsWillBeLoaded") private var configurationsWillBeLoaded: [URL] = []
+    @AppStorage("configurationsWillBeLoadedBookmarks") private var configurationsWillBeLoadedBookmarks: [Data] = []
 
     init() {
         initLibipfErrorHandler()
 
         if loadConfigurationsOnStartup {
-            for configFile in configurationsWillBeLoaded {
+            for (index, bookmarkData) in configurationsWillBeLoadedBookmarks.enumerated() {
+                var stale = false
                 do {
-                    var jsonString: String
-                    if #available(macOS 13, *) {
-                        jsonString = try String(contentsOfFile: configFile.path(percentEncoded: false))
-                    } else {
-                        jsonString = try String(contentsOfFile: configFile.path)
+                    let configFileURL = try URL(
+                        resolvingBookmarkData: bookmarkData,
+                        options: [.withSecurityScope],
+                        relativeTo: nil,
+                        bookmarkDataIsStale: &stale
+                    )
+
+                    if stale {
+                        let newBookmarkData = try configFileURL.bookmarkData(
+                            options: [.withSecurityScope],
+                            includingResourceValuesForKeys: nil,
+                            relativeTo: nil
+                        )
+                        configurationsWillBeLoadedBookmarks[index] = newBookmarkData
                     }
 
-                    let list = try JSONDecoder().decode([ForwardedItemInfo].self, from: jsonString.data(using: .utf8)!)
+                    if configFileURL.startAccessingSecurityScopedResource() {
+                        defer { configFileURL.stopAccessingSecurityScopedResource() }
 
-                    for item in list {
-                        globalState.items.append(try ForwardedItem(item: item))
+                        let jsonString: String
+                        if #available(macOS 13, *) {
+                            jsonString = try String(contentsOfFile: configFileURL.path(percentEncoded: false))
+                        } else {
+                            jsonString = try String(contentsOfFile: configFileURL.path)
+                        }
+
+                        let list = try JSONDecoder().decode([ForwardedItemInfo].self, from: jsonString.data(using: .utf8)!)
+
+                        for item in list {
+                            globalState.items.append(try ForwardedItem(item: item))
+                        }
+                    } else {
+                        showErrorDialog("Unable to access the configuration file. Please ensure the file exists and that you have granted the necessary permissions.")
                     }
                 } catch {
                     showErrorDialog(error)
